93a3c27d8193744ff34601e7fdeb751ba079fe2d
Daniel <daniel.n96@t-online.de>
2019-03-12 22:15:37 +0100

    Created Deck

--- src/data/Card.java
+++ src/data/Card.java
@@ -2,23 +2,60 @@ package data;
 
 public class Card {
 
-    private Colour colour;
-    private Value value;
+    private final Colour colour;
+    private final Value value;
+
+    public Card(Colour c, Value v){
+        this.colour = c;
+        this.value = v ;
+    }
 
     public Colour getColour() {
         return colour;
-    }
-
-    public void setColour(Colour colour) {
-        this.colour = colour;
     }
 
     public Value getValue() {
         return value;
     }
 
-    public void setValue(Value value) {
-        this.value = value;
+    public String toString(){
+        return getShowValue() + "("+colour+") ";
     }
+
+    private String getShowValue() {
+        switch (value) {
+            case TWO:
+                return "2";
+            case THREE:
+                return "3";
+            case FOUR:
+                return "4";
+            case FIVE:
+                return "5";
+            case SIX:
+                return "6";
+            case SEVEN:
+                return "7";
+            case EIGEHT:
+                return "8";
+            case NINE:
+                return "9";
+            case TEN:
+                return "T";
+            case JACK:
+                return "J";
+            case QUEEN:
+                return "Q";
+            case KING:
+                return "K";
+            case ACE:
+                return "A";
+            default:
+                return"X";
+
+        }
+    }
+
+
 
 }
--- src/game/PokerAi.java
+++ src/game/PokerAi.java
@@ -1,8 +1,14 @@
 package game;
 
+import data.Deck;
+
 public class PokerAi {
     public static void main(String[] args) {
         System.out.println("--> PokerAI <--");
+        Deck deck = new Deck();
+
+        System.out.println(deck.getCard().toString());
+        System.out.println(deck.getCard().toString());
 
     }
 }
