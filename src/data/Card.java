33e402735c57558912502df6f2b8ba422eb172d9
Daniel <daniel.n96@t-online.de>
2019-03-12 20:16:37 +0100

    Created Card, Value, Coluor

---
+++ src/data/Card.java
@@ -0,0 +1,24 @@
+package data;
+
+public class Card {
+
+    private Colour colour;
+    private Value value;
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public void setColour(Colour colour) {
+        this.colour = colour;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+}
---
+++ src/data/Value.java
@@ -0,0 +1,7 @@
+package data;
+
+public enum Value { TWO,THREE, FOUR, FIVE,
+    SIX, SEVEN, EIGEHT, NINE, TEN, JACK,
+    QUEEN, KING, ACE
+
+}
