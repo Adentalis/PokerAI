45b42ccb4b86be58ed05a1e7dc45956fcc21ce5b
Daniel <daniel.n96@t-online.de>
2019-03-17 15:35:14 +0100

    test

--- src/game/PokerAi.java
+++ src/game/PokerAi.java
@@ -4,7 +4,7 @@ import data.Deck;
 
 public class PokerAi {
     public static void main(String[] args) {
-        System.out.println("--> PokerAI <--");
+        System.out.println("--> PokerAI <--t");
         Deck deck = new Deck();
 
         for(int i = 1 ; i <= 52 ; i ++) {
